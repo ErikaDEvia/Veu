14783bca2246b196ffb583c4e9837f059f4fc968
ErikaDEvia <lorenaprada4981@gmail.com>
2025-02-15 09:52:40 -0500

    Agregando archivos al GitHub

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Welcome">
+    <h1>Welcome to page</h1>
+    <h3>Ahora eres parte de nuestro equipo!</h3>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .welcome {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
